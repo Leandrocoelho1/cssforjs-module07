e7a7ea8b895b688887a1ecbac6038e3d2569b2e1
Leandro Arzolla Coelho <leandro.coelho@farfetch.com>
2022-01-17 17:35:18 -0300

    solved exercise

---
+++ src/components/Header/Header.js
@@ -0,0 +1,137 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
+
+import { QUERIES } from "../../constants";
+
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
+
+const Header = () => {
+  return (
+    <header>
+      <SuperHeader>
+        <Row>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+          <ActionGroup>
+            <button>
+              <User size={24} />
+            </button>
+          </ActionGroup>
+        </Row>
+      </SuperHeader>
+      <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
+        <Logo />
+        <AuthWrapper>
+          <Side />
+          <Button>Subscribe</Button>
+          <Side>
+            <SubLink href="#">Already a subscriber?</SubLink>
+          </Side>
+        </AuthWrapper>
+      </MainHeader>
+    </header>
+  );
+};
+
+const SuperHeader = styled.div`
+  padding: 16px 0;
+  background: var(--color-gray-900);
+  color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const Row = styled(MaxWidthWrapper)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ActionGroup = styled.div`
+  display: flex;
+  gap: 24px;
+
+  /*
+    FIX: Remove the inline spacing that comes with
+    react-feather icons.
+  */
+  svg {
+    display: block;
+  }
+`;
+
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const AuthWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    height: 100%;
+    justify-self: end;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+const Side = styled.div`
+  flex: 1;
+`;
+
+const SubLink = styled.a`
+  font-size: 0.875rem;
+  color: var(--color-gray-900);
+  font-style: italic;
+  text-decoration: underline;
+`;
+
+const MainHeader = styled(MaxWidthWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+  margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    /* trick to keep logo centered */
+    grid-template-columns: 1fr auto 1fr;
+    align-items: center;
+    justify-items: start;
+    justify-items: revert;
+
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
+`;
+
+export default Header;
